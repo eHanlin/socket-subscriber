9cf75ab72069d39d93b77a0b3dddc17ab417f59d
Sparrow.jang <sparrow.jang@gmail.com>
2016-10-26 10:42:37 +0800

    added a production env

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,14 @@
-var path = require('path')
+var path = require('path');
+var entry = ['./src/index'];
+
+if (process.env.NODE_ENV === undefined) {
+  entry.unshift('webpack/hot/only-dev-server');
+  entry.unshift('webpack-dev-server/client?http://localhost:8080');
+}
 
 module.exports = { 
   devtool: 'cheap-module-eval-source-map',
-  entry: [
-    'webpack-dev-server/client?http://localhost:8080',
-    'webpack/hot/only-dev-server',
-    './src/index'
-  ],  
+  entry: entry,
   output: {
     path: path.join(__dirname, 'build'),
     filename: 'bundle.js'
